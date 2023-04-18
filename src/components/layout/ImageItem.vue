f0e3a19c0eb24f39b43ff27569c6d84cc0700d53
mateusz2247 <mateusz2247@gmail.com>
2023-04-18 21:15:08 +0200

    migajacy mouseover

--- src/components/layout/ImageItem.vue
+++ src/components/layout/ImageItem.vue
@@ -1,5 +1,10 @@
 <template>
-	<img :src="imageSrc" class="image" />
+	<div class="colored" v-show="isHovered"></div>
+	<img
+		@mouseover="mouseOver"
+		@mouseleave="mouseOver"
+		:src="imageSrc"
+		class="image" />
 </template>
 
 <script>
@@ -7,12 +12,21 @@ import { imagesUrl } from "@/config";
 
 export default {
 	name: "ImageItem",
+	data() {
+		return {
+			isHovered: false,
+		};
+	},
 	props: {
 		src: {
 			type: String,
 			required: true,
 		},
 	},
+	methods: {
+        mouseOver: function(){
+            this.isHovered = !this.isHovered;   
+        }},
 	computed: {
 		imageSrc() {
 			return `${imagesUrl}/${this.src}`;
@@ -24,6 +38,15 @@ export default {
 <style lang="scss">
 .image {
 	height: 350px;
+	width: 220px;
 	object-fit: cover;
+	position: relative;
+}
+.colored {
+	height: 350px;
+	width: 100%;
+	background-color: rgba(155, 89, 182, 0.5);
+	position: absolute;
+	z-index: 100;
 }
 </style>
